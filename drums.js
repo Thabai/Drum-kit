8e0a58c4a9620531f740769714cfb46b231825e0
Carly Morris <thabai@hotmail.com>
2021-06-16 13:34:08 +0100

    commit

---
+++ drums.js
@@ -0,0 +1,11 @@
+//sounds
+let boom = document.getElementById("boom");
+let clap = document.getElementById("clap");
+let hihat = document.getElementById("hihat");
+let kick = document.getElementById("kick");
+let openhat = document.getElementById("openhat");
+let ride = document.getElementById("ride");
+let snare = document.getElementById("snare");
+let tink = document.getElementById("tink");
+let tom = document.getElementById("tom");
+
