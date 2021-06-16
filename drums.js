8b56b49298ddce7e1ff3894fd25b5b9aeaa05998
Carly Morris <thabai@hotmail.com>
2021-06-16 16:26:02 +0100

    commit

--- drums.js
+++ drums.js
@@ -1,11 +1,27 @@
-//sounds
-let boom = document.getElementById("boom");
-let clap = document.getElementById("clap");
-let hihat = document.getElementById("hihat");
-let kick = document.getElementById("kick");
-let openhat = document.getElementById("openhat");
-let ride = document.getElementById("ride");
-let snare = document.getElementById("snare");
-let tink = document.getElementById("tink");
-let tom = document.getElementById("tom");
 
+
+
+
+function playSound(e) {
+    // Get element that matches the key pressed
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return; // Stop function from running all together
+
+    audio.currentTime = 0; // Rewind the audio
+    audio.play();
+    key.classList.add('playing');
+}
+
+// Removes the transition effect 
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return; // skip if it is not a transform
+    this.classList.remove('playing');
+}
+
+// Listening on each key for when the CSS transition ends
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+// We will listen to the event of the keys pressed
+window.addEventListener('keydown', playSound);
