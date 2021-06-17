e41fe6540a0efcbaefb56f2b57331b11d43ea0f9
Carly Morris <thabai@hotmail.com>
2021-06-17 17:46:55 +0100

    commit

--- script/drums.js
+++ script/drums.js
@@ -2,31 +2,35 @@
 function playSounds(e) {
     // Get element that matches the key pressed
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
     if (!audio) return; // Stop function from running all together
+    this.playSound(audio.id);
 
-    audio.currentTime = 0; // Rewind the audio
-    audio.play();
-    key.classList.add('playing');
+}
+function bigSmall(e) {
+    if (!e) return;
+    e.style.transform = "scale(1.1)";
+    setTimeout(function() {
+        e.style.transform = "scale(1)"; 
+    }, 100);
 }
 
-// Removes the transition effect 
-function removeTransition(e) {
-    if (e.propertyName !== 'transform') return; // skip if it is not a transform
-    this.classList.remove('playing');
+function playSound(part) {
+    
+    const sound = document.getElementById(part);
+    const pic = document.getElementById('pic-'+part);
+    const key = document.getElementById('key-'+part);
+
+    this.bigSmall(key);
+    this.bigSmall(pic);
+    
+    sound.currentTime = 0;
+    sound.play();
+  
 }
 
-// Listening on each key for when the CSS transition ends
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
 // We will listen to the event of the keys pressed
 window.addEventListener('keydown', playSounds);
-// window.addEventListener('click' )
+window.addEventListener ('click', playSound);
 
-
-
-function playSound(part) {
-    const sound = document.getElementById(part);
-    sound.play();
-}
